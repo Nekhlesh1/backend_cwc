907a8a1dfc0fc219addbdb24563dba438fb2a5d3
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2024-02-17 13:26:21 +0530

    connected to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-// const PORT = 8000;
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
 
-app.get('/', (req,res)=> {
-    res.send("Hi")
+dotenv.config({
+    path: './env'
 })
+connectDB();
 
-app.listen(process.env.PORT, () =>{
-    console.log("Listening to port : ",process.env.PORT);
-})
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express"
+// const app = express();
+
+// (async ()=>{
+
+//     try {
+//         mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERRRRR", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=> {
+//             console.log("Listening on port",process.env.PORT);
+//         })
+        
+//     }
+//     catch(error) {
+//         console.error("ERROR :", error)
+//         throw error
+
+//     }
+// })()
