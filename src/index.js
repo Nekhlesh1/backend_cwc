ce5528d673c81b4117b4ac60fbe95f7b0b635fc9
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2024-02-21 13:30:50 +0530

    Error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // require('dotenv').config()
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import app from './app.js'
 
 dotenv.config({
     path: './env'
+});
+connectDB().
+then(()=> {
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`Successfully connected to server on port ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Mongo db connection failed!!",err)
 })
-connectDB();
 
 
 
