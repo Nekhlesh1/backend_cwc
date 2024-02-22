74dc02ef93693199b060df89d6134d785c9be962
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2024-02-23 00:10:26 +0530

    implemented jwt, bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import { Video } from "./video.model";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     watchHistory :
@@ -55,5 +56,48 @@ const userSchema = new mongoose.Schema({
 {
     timestamps : true
 });
+userSchema.pre("save",async function(next) {
+    if(!this.isModified) 
+    {
+        return next();
+
+    }
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+
+});
+
+userSchema.methods.isPasswordCorrect = async function(password)
+{
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.methods.generateAccessToken = function() 
+{
+   return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+            fullName : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() 
+{
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const  User = mongoose.model("User", userSchema);
